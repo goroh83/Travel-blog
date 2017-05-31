08b7f8db5843660932643038477715ea4ee899be
goroh83 <goroh83@yahoo.com>
2017-05-31 12:30:17 +0200

    instagram img size small scr fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // navigation resize on scroll
-$(document).on('scroll', function(){
-    if($(document).scrollTop()>50) {
+$(document).on('scroll', function () {
+    if ($(document).scrollTop() > 50) {
         $('header').removeClass('large').addClass('small');
     } else {
         $('header').removeClass('small').addClass('large');
     }
 });
 
-$('.compas').on('click', function(){
+$('.compas').on('click', function () {
     $('.menu').toggleClass('opacity');
 });
 
 
-$(window).scroll(function() {
+$(window).scroll(function () {
 
-var wScroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
 
-if(wScroll > $('.photos').offset().top - ($(window).height() / 1.3)) {
+    if (wScroll > $('.photos').offset().top - ($(window).height() / 1.3)) {
 
-    $('.photos figure').each(function(i){
+        $('.photos figure').each(function (i) {
 
-        setTimeout(function(){
-        $('.photos figure').eq(i).addClass('showing');
-        }, 100 *(i+1));
-    });
-}
+            setTimeout(function () {
+                $('.photos figure').eq(i).addClass('showing');
+            }, 100 * (i + 1));
+        });
+    }
 })
